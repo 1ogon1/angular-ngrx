040168dc47fef48c28e307baed0615a374cd85d8
1ogon1 <konovalenkoruslan@gmail.com>
2021-09-15 00:02:40 +0300

    lesson 11

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Store } from '@ngrx/store'
+import { registerAction } from '../../store/actions/register.action'
+
+@Component({
+  selector: 'an-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  form: FormGroup | undefined
+
+  constructor(private formBuilder: FormBuilder, private store: Store) {}
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  initializeForm(): void {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: '',
+      password: '',
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.form?.value)
+    this.store.dispatch(registerAction(this.form?.value))
+  }
+}
