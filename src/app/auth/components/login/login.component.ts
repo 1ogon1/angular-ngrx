a7bb96639020b3ea8f45da33e7b489247e60a27c
Руслан Коноваленко <konovalenko.ruslan@es.lan>
2021-09-17 18:16:09 +0300

    lesson 18

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,7 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'an-login',
     templateUrl: './login.component.html',
     styleUrls: ['login.component.scss']
 })
+export class LoginComponent implements OnInit {
+
+    constructor(private storage: Storage) {
+        
+    }
+
+    ngOnInit(): void {
+
+    }
+}
